1acddc990233be8859bb136857d04087fa25ad30
mdasifmunshe <asif35-2872@diu.edu.bd>
2023-12-19 16:52:22 +0600

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold p-0 m-0">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App
